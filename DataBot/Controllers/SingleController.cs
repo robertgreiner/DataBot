db0d202feae447c7353ab4d0acd739cd03ddb215
Robert Greiner <robert@robertgreiner.com>
2011-03-19 13:58:34 -0500

    View details on a single random row

---
+++ DataBot/Controllers/SingleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataBot.Models;
+
+namespace DataBot.Controllers
+{
+    public class SingleController : Controller
+    {
+        //
+        // GET: /Single/
+
+        public ActionResult Index()
+        {
+            return View(new RandomDataRow());
+        }
+
+    }
+}
